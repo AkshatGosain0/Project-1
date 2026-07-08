0e160c5d839c85d3e77a3330e71edfbc5c588f42
AkshatGosain0 <akshatgosain0@gmail.com>
2026-07-08 13:22:55 +0100

    Issue solved of conversion error

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
 # this is the introduction and input section of code
 print("-----Welcome to the caclulator-----")
-num1 =input("--Enter your first number-- \t")
-num2 =input("--Enter your second number-- \t")
+num1 =int(input("--Enter your first number-- \t"))
+num2 =int(input("--Enter your second number-- \t"))
 operation = input("Choose between these operations: \n + \n - \n * \n / \n")
 
 # this section will be the logical section in which operation performed based on option chosen
