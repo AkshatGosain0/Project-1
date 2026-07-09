d9cd75eaf14e87a3bdf920881bc690903450870c
AkshatGosain0 <akshatgosain0@gmail.com>
2026-07-09 13:22:18 +0100

    More operations have been added

--- Calculator.py
+++ Calculator.py
@@ -2,24 +2,29 @@
 print("-----Welcome to the caclulator-----")
 num1 =int(input("--Enter your first number-- \t"))
 num2 =int(input("--Enter your second number-- \t"))
-operation = input("Choose between these operations: \n + \n - \n * \n / \n")
+operation = input(("Choose between these operations: \n (+) Addition \n (-) Subtraction \n (*) Multiplication \n (/) Division \n (^) Power \n (R) Remainder \n")).strip() # adding more part of new branch
 
 # this section will be the logical section in which operation performed based on option chosen
 #adding a while loop for exception handling, part of new branch
-while operation not in ["+" , "-" , "*" , "/"]:
+while operation not in ["+" , "-" , "*" , "/" , "^" , "R"]:
     print("Invalid operation chosen")
-    operation = operation = input("Choose between these operations: \n + \n - \n * \n / \n")
+    operation = (input("Choose between these operations: \n (+) Addition \n (-) Subtraction \n (*) Multiplication \n (/) Division \n (^) Power \n (R) Remainder \n")).strip()
 
-    if operation == "+":
-        sum = num1 + num2
-        print(f"The answer is {sum}")
-    elif operation == "-":
-        sum = num1 - num2
-        print(f"The answer is {sum}")
-    elif operation == "*":
-        sum = num1 * num2
-        print(f"The answer is {sum}")
-    elif operation == "/":
-        sum = num1 / num2
-        print(f"The answer is {sum}")
-    
+if operation == "+":
+    sum = num1 + num2
+    print(f"The answer is {sum}")
+elif operation == "-":
+    sum = num1 - num2
+    print(f"The answer is {sum}")
+elif operation == "*":
+    sum = num1 * num2
+    print(f"The answer is {sum}")
+elif operation == "/":
+    sum = num1 / num2
+    print(f"The answer is {sum}")
+elif operation == "^":
+    sum = num1**num2
+    print(f"The answer is {sum}")
+elif operation == "R":
+    sum = num1 % num2
+    print(f"The answer is {sum}")
